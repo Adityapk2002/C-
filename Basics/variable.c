62f5f4c2c36f919719b685391d10011dfb023062
aditya khandade <adityakhandade2002@gmail.com>
2025-08-24 23:23:30 +0530

    a

--- Basics/variable.c
+++ Basics/variable.c
@@ -2,6 +2,13 @@
 int main()
 {
     int age = 22;
-    printf("My age is %d", age);
+    printf("My age is %d\n", age);
+
+    char firstNameCharacter = 'A';
+    printf("My firstNameCharacter is %c\n", firstNameCharacter);
+
+    char name[50] = "Aditya Khandade";
+    printf("Size of character is %ld\n", sizeof(char));
+    printf("My name is %s\n", name);
     return 0;
 }
